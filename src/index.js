798c4466824bb165d4cfcf2218eab0a0fc624474
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-08-31 20:55:54 +0100

    complete webpack set up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
