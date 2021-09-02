6ca612ac5bcb6f86717f5e6cc8cfec2b66884225
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-09-02 11:25:30 +0100

    add Dom manipulations

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+import game, { getScores, submitScore } from './addScore';
+import displayScores from './showScore';
+
+
+const scoresList = document.getElementById('scoreboard');
+
+const loadScores = () => getScores().then((scores) => {
+  const html = displayScores(scores);
+  if (html) {
+    scoresList.innerHTML = html;
+    scoresList.classList.add('borders');
+  } else {
+    scoresList.classList.remove('borders');
+  }
+});
+
+
+const refreshButton = document.getElementById('refresh-scoreboard');
+refreshButton.addEventListener('click', loadScores);
+window.onload = () => {
+  const title = document.getElementById('page-title');
+  title.innerHTML += ` - ${game}`;
+  loadScores();
+};
+
+
+const newScore = document.getElementById('new-score');
+const newplayer = document.getElementById('player-name');
+const newplayerScore = document.getElementById('player-score');
+
+newScore.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const player = newplayer.value;
+  const score = newplayerScore.value;
+  const scoreError = document.querySelector('.error-msg');
+  if (player && score && !Number.isNaN(score)) {
+    scoreError.classList.add('hidden');
+    submitScore(player, parseInt(score, 10)).then(() => {
+      newplayerScore.value = '';
+      newplayer.value = '';
+    });
+  } else {
+    newplayerScore.value = '';
+    newplayer.value = '';
+    scoreError.classList.remove('hidden');
+  }
+});
